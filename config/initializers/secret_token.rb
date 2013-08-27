ecd60a2c9875f417d9c9d461a1dfa00775eacd89
Baozi <baozi.rails@gmail.com>
2013-08-27 14:04:47 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsForZombie::Application.config.secret_key_base = '602fedc4da473f00233fe4b4c419c7a0cf420ddbeb3f2165380fe7d5c8da12218d7b97caba76548d1bbdf9336b55e7c3885574aa49be7047b93c507ed97cc7b0'
